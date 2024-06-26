2773a30672cc6b0359707801eb6acb2c26b98cd3
SBMcodes <shreyankumar212@gmail.com>
2024-06-26 16:53:47 +0530

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,26 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      backgroundImage: {},
+      textColor: {
+        skin: {
+          base: "var(--color-text-base)",
+          muted: "var(--color-text-muted)",
+        },
+      },
+      backgroundColor: {
+        skin: {
+          fill: "var(--color-fill)",
+          "button-accent": "var(--color-button-accent)",
+          "button-accent-hover": "var(--color-button-accent-hover)",
+        },
+      },
+      // can be used like: from-skin-start
+      gradientColorStops: {
+        skin: {
+          start: "var(--color-fill)",
+          end: "var(--color-fill-inverted)",
+        },
       },
     },
   },
